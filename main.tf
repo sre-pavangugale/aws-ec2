7e8697587c0d7d923ef476342d43f4edee81e3a2
sre-pavangugale <devops.pavangugale@gmail.com>
2026-01-09 13:52:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test" {
     ami           = "ami-0ced6a024bb18ff2e"  # Specify an appropriate AMI ID
     instance_type = "t3.micro"
 }
