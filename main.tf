e80996d0a0ecdac137a256fa3367b909756cbe8d
sre-pavangugale <devops.pavangugale@gmail.com>
2026-01-09 13:48:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "example" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  name = "test"
   ami_value = "ami-0ced6a024bb18ff2e" # replace this
   instance_type_value = "t3.micro"
   subnet_id_value = "subnet-0ee60a2e12094eb23" # replace this
